44769bc0d9382b382b1b8015c771a5680bff5351
wenops <671313512@qq.com>
2019-04-25 18:40:35 +0800

    Update runtest.py

--- runtest.py
+++ runtest.py
@@ -21,7 +21,7 @@ def send_mail(file_new):
     smtpserver = 'smtp.qq.com'
     # 发送邮箱用户/密码
     user = '671313512'
-    password = 'oiesdcrcgrtqbcdb'
+    password = '*****'
     # 发送邮箱
     sender = '671313512@qq.com'
 
